d1610896237e94f6f807d572a14bd11f2f49ab1a
Franco <franchito2008@icloud.com>
2025-03-26 15:32:30 +0100

    top card

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,27 +1,35 @@
-module.exports = {
-    theme: {
-      extend: {
-        colors: {
-          'light-red': 'hsl(0, 100%, 67%)',
-          'orangey-yellow': 'hsl(39, 100%, 56%)',
-          'green-teal': 'hsl(166, 100%, 37%)',
-          'cobalt-blue': 'hsl(234, 85%, 45%)',
 
-          'white': 'hsl(0, 0%, 100%)',
-          'pale-blue': 'hsl(221, 100%, 96%)',
-          'light-lavender': 'hsl(241, 100%, 89%)',
-          'dark-gray-blue': 'hsl(224, 30%, 27%)',
-        },
-        // Farbverläufe
-        backgroundImage: {
-          'light-slate-blue': 'hsl(252, 100%, 67%)',
-          'light-royal-blue': 'hsl(241, 81%, 54%)',
-          'violet-blue': 'hsla(256, 72%, 46%, 1)',
-          'persian-blue': 'hsla(241, 72%, 46%, 0)',
-        },
-        fontFamily: {
-            'hanken': ['Hanken Grotesk', 'sans-serif'],
-          },
+import './index.css';
+
+module.exports = {
+  content: [
+    "./public/index.html",  // Stelle sicher, dass der Pfad zu deinen HTML/JS-Dateien stimmt
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'light-red': 'hsl(0, 100%, 67%)',
+        'orangey-yellow': 'hsl(39, 100%, 56%)',
+        'green-teal': 'hsl(166, 100%, 37%)',
+        'cobalt-blue': 'hsl(234, 85%, 45%)',
+        'white': 'hsl(0, 0%, 100%)',
+        'pale-blue': 'hsl(221, 100%, 96%)',
+        'light-lavender': 'hsl(241, 100%, 89%)',
+        'dark-gray-blue': 'hsl(224, 30%, 27%)',
+      },
+      backgroundImage: {
+        'light-slate-blue': 'hsl(252, 100%, 67%)',
+        'light-royal-blue': 'hsl(241, 81%, 54%)',
+        'violet-blue': 'hsla(256, 72%, 46%, 1)',
+        'persian-blue': 'hsla(241, 72%, 46%, 0)',
+      },
+      fontFamily: {
+        'hanken': ['Hanken Grotesk', 'sans-serif'],
       },
     },
-  }
+  },
+  plugins: [
+    require('tailwindcss'),
+    require('autoprefixer'),
+  ],
+}
